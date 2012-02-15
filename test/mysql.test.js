8ac61d303d269262946e55109c3a60fdcbdac68c
Travis Parks <jehugaleahsa@gmail.com>
2012-02-14 22:54:38 -0500

    Started testing with Mocha. No useful tests yet.

---
+++ test/mysql.test.js
@@ -0,0 +1,9 @@
+var mysql = require('../mysql.js');
+
+describe('MySqlClient', function () {
+    describe('#select(query, parameters, callback)', function() {
+        it('should do something', function () {
+            // perform test
+        });
+    });
+});
